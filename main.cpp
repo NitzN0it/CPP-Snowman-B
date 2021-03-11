83e9bd2a5734b40454249aa5675bbfd368a21d53
NitzN0it <nitz@namer.us>
2021-03-11 16:38:12 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by nitzan on 11/03/2021.
+//
+#include "snowman.hpp"
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+int main ()
+{
+    for (int i = 0; i < 5000; ++i) {
+        cout << "\x1B[2J\x1B[H" << endl;
+        cout << ariel::snowman(12341234) +"\n\n "+ariel::snowman(42332222) +"\n\n "+ ariel::snowman(44331144)<< endl;
+        usleep(100);
+        cout << "\x1B[2J\x1B[H" << endl;
+        cout << ariel::snowman(12341334) +"\n\n "+ariel::snowman(42333322) +"\n\n "+ ariel::snowman(44111144)<< endl;
+        usleep(100);
+    }
+}
