7a0a32c351e2754dee5fe5881b02a55803a03e0f
NitzN0it <nitz@namer.us>
2021-03-11 17:13:20 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@
 using namespace std;
 int main ()
 {
-    for (int i = 0; i < 5000; ++i) {
+    for (int i = 0; i < 4000; ++i) {
         cout << "\x1B[2J\x1B[H" << endl;
-        cout << ariel::snowman(12341234) +"\n\n "+ariel::snowman(42332222) +"\n\n "+ ariel::snowman(44331144)<< endl;
-        usleep(100);
+        cout << " "+ariel::snowman(12341234) +"\n\n "+ariel::snowman(42332222) +"\n\n "+ ariel::snowman(44331144)<< endl;
+        usleep(100000);
         cout << "\x1B[2J\x1B[H" << endl;
-        cout << ariel::snowman(12341334) +"\n\n "+ariel::snowman(42333322) +"\n\n "+ ariel::snowman(44111144)<< endl;
-        usleep(100);
+        cout << " "+ariel::snowman(12341334) +"\n\n "+ariel::snowman(42333322) +"\n\n "+ ariel::snowman(44111144)<< endl;
+        usleep(100000);
     }
 }
